1b461ec55572be86b0f780e56eba9ed04f479b36
Nick Lanese <nlanese21@gmail.com>
2020-02-27 22:20:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,17 +12,17 @@ WIN_COMBINATIONS = [
 ]
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
-end
+end #ends method
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+end #ends method
 def input_to_index(user_input)
   user_input.to_i - 1
-end
+end #end method
 def valid_move?(board, index)
   if (position_taken?(board, index))
     return false
@@ -31,12 +31,12 @@ def valid_move?(board, index)
       return false
     else
       return true
-    end
-  end
-end
+    end #Ends check to see if index is on the board
+  end #ends check to see if position_taken
+end #ends methods
 def move(board, input, player)
   board[input] = player
-end
+end #ends method
 def turn(board)
   puts "Please enter 1-9: "
   index = gets.strip
@@ -46,8 +46,8 @@ def turn(board)
   else
     puts "Invalid move!"
     turn(board)
-  end
-end
+  end #ends check to see if valid move
+end #ends method
 def won?(board)
   WIN_COMBINATIONS
   count = 0
@@ -73,61 +73,61 @@ def won?(board)
     end # ends win or continue
   end # ends the until loop
   return nil
-end
+end #ends method
 def full?(board)
   full = board.all? do |space|
     (space == "X" || space == "O")
-  end
+  end #ends full check loop
   return full
-end
+end #ends method
 def draw?(board)
   if (full?(board))
     if (won?(board))
       return false
     else
       return true
-    end
+    end #ends to check if game was won
   else
     return false
-  end
-end
+  end #ends to see if board is full
+end #ends method
 def turn_count(board)
   turns = 0
   board.each do |spot|
   if (spot == "X" || spot == "O")
     turns += 1
-  end
+  end #ends counting filled spots
   return turns
-end
+end #ends method
 def current_player(board)
   if (turn_count(board) % 2 == 0)
     return "X"
   else
     return "O"
-  end
-end
+  end #ends determining X or O
+end #ends method
 def over?(board)
   if (draw?(board) || won?(board))
     return true
   else
     return false
-  end
-end
+  end #ends seeing if won or draw
+end #ends method
 def winner(board)
   winning_array = won?(board)
   if winning_array == nil
     return nil
-  end
+  end #ends checking winning array
   space = winning_array[0]
   return board[space]
-end
+end #ends method
 def play(board)
   until (over?(board))
     turn(board)
-  end
+  end #ends loop
   if (draw?(board))
     puts "Cat's Game!"
   elsif (won?(board))
     puts "Congratulations #{winner(board)}!"
-  end
-end
+  end #ends checking how game is won
+end #ends method
